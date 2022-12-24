73d089562c3f1b33d57c19ae9d3b5fb48986dfe5
Lukas Krickl <lukas@krickl.dev>
2022-12-24 15:19:06 +0100

    Added more parser

--- src/expr.rs
+++ src/expr.rs
@@ -1,9 +1,9 @@
 use std::io::Read;
 
-use crate::{Error, RbrepResult};
+use crate::{Error, Parser, RbrepResult};
 
 pub enum Expr {
-    Byte,
+    Byte { value: u8 },
     Any,
     Group,
     String,
@@ -12,8 +12,33 @@ pub enum Expr {
 pub type ExprBranch = Vec<Expr>;
 
 impl Expr {
-    pub fn expr_tree_from(src: &str) -> RbrepResult<ExprBranch> {
-        Err(Error::Unknown)
+    pub fn tree_from(src: &str) -> RbrepResult<ExprBranch> {
+        let mut parser = Parser::new(src);
+        Self::tree_from_parser(&mut parser)
+    }
+
+    fn tree_from_parser(parser: &mut Parser) -> RbrepResult<ExprBranch> {
+        let mut branch: ExprBranch = vec![];
+        branch.push(Self::parse(parser)?);
+        Ok(branch)
+    }
+
+    fn parse_byte(parser: &mut Parser, first: char) -> RbrepResult<Expr> {
+        let second = parser.next();
+        let value = u8::from_str_radix(&format!("{}{}", first, second), 16)
+            .map_err(|_| Error::BadSyntax)?;
+
+        Ok(Expr::Byte { value })
+    }
+
+    fn parse(parser: &mut Parser) -> RbrepResult<Expr> {
+        let first = parser.next();
+
+        if first.is_ascii_hexdigit() {
+            Self::parse_byte(parser, first)
+        } else {
+            Err(Error::Unknown)
+        }
     }
 
     pub fn apply(&self, f: &mut dyn Read) -> RbrepResult<()> {
